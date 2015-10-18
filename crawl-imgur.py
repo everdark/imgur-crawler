c40cf612a452a2f7e68e6a2b4ce1c6b9ea8efb69
Kyle Chung <kylechun9@gmail.com>
2015-10-18 18:03:48 +0800

    add status bar

--- crawl-imgur.py
+++ crawl-imgur.py
@@ -3,6 +3,8 @@
 
 import argparse
 import os
+import sys
+import time
 import re
 import shutil
 import requests
@@ -18,9 +20,11 @@ def getArgParser():
                         help="the destination folder to save image")
     parser.add_argument("-n", "--nitem", metavar="N", type=int, action="store",
                         help="the maximum number of images to save")
+    parser.add_argument("-s", "--show-status", action="store_true", default=False,
+                        help="optionally show download status")
     return parser
 
-def crawlImgur(src_url, dest_path, nitem):
+def crawlImgur(src_url, dest_path, nitem, show_status=True):
     soup = BeautifulSoup(requests.get(src_url).content, "lxml")
     tlist = soup.findAll("meta", {"property": "og:image"})
     
@@ -33,19 +37,33 @@ def crawlImgur(src_url, dest_path, nitem):
     for t in tlist:
         img_src = t.get("content")
         if re.search("\.jpg$", img_src):
-            print "download %s..." % img_src,
             r = requests.get(img_src, stream=True)
             outfname = os.path.join(dest_path, os.path.basename(img_src))
-            with open(outfname, 'wb') as outfile:
-                shutil.copyfileobj(r.raw, outfile)
-                print "done"
+            with open(outfname, "wb") as outfile:
+                if show_status:
+                    print "download %s..." % img_src,
+                    size_downloaded = 0
+                    size_total = len(r.content)
+                    for chunk in r.iter_content(chunk_size=8192):
+                        if chunk:
+                            outfile.write(chunk)
+                            size_downloaded += len(chunk)
+                            status = r"%10d  [%3.2f%%]" % (size_downloaded, size_downloaded * 100. / size_total)
+                            status = status + chr(8)*(len(status)+1)
+                            print status,
+                            sys.stdout.flush()
+                    print ''
+                else:
+                    print "download %s..." % img_src,
+                    shutil.copyfileobj(r.raw, outfile)
+                    print "done"
 
     return None
 
 def main():
     parser = getArgParser()
     args = parser.parse_args()
-    crawlImgur(args.url, args.dest, args.nitem)
+    crawlImgur(args.url, args.dest, args.nitem, args.show_status)
 
 if __name__ == "__main__":
     main()
